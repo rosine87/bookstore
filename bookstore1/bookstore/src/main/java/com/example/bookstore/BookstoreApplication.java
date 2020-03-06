4ed1ab7b6be523a89c063889fd878798b8315686
Utente <Utente@DESKTOP-CFSQDJU.homenet.telecomitalia.it>
2020-03-06 17:13:14 +0100

    Initial commit. created spring boot project and configure the application properties

---
+++ bookstore1/bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+		//System.out.print("hello world");
+	}
+
+}
